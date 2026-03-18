814f08bb03ffc622f4db391d4dec9ad9badac922
Sereti <httxsereti@gmail.com>
2026-03-18 20:22:16 +0100

    fix(units): fix magic number using operators

--- apps/app/src/utils/calculate_magic_number.py
+++ apps/app/src/utils/calculate_magic_number.py
@@ -22,36 +22,35 @@ def calculate_magic_number(current_value: int, operators: str) -> int:
 
     # match +[5-10], -[5-10], %+[5-10], %-[5-10]
     if match := re.match(r"^(%*[+\-])\[(\d+)-(\d+)\]$", operators):
+        op = match.group(1)
+
         low, high = int(match.group(2)), int(match.group(3))
         rand_val = random.randint(min(low, high), max(low, high))
 
-        if match.group(1) == "+":
+        if op == "+":
             return min(current_value + rand_val, 99)
-        elif match.group(1) == "-":
+        elif op == "-":
             return max(current_value - rand_val, 0)
-        elif match.group(1) == "%+":
+        elif op == "%+":
             return min(current_value + math.ceil(current_value * rand_val / 100), 99)
-        elif match.group(1) == "%-":
+        elif op == "%-":
             return max(current_value - math.ceil(current_value * rand_val / 100), 0)
 
     # match +5, %+5, -5, %-5, 5
-    if match := re.match(r"(%*[\\+,-]*)([1-9]*\d)$", operators):
-        if match.group(1) == "+":
-            new_value = min(current_value + int(match.group(2)), 99)
-        elif match.group(1) == "-":
-            new_value = max(current_value - int(match.group(2)), 0)
-        elif match.group(1) == "%+":
-            new_value = min(
-                current_value + math.ceil(current_value * int(match.group(2)) / 100),
-                99,
-            )
-        elif match.group(1) == "%-":
-            new_value = max(
-                current_value - math.ceil(current_value * int(match.group(2)) / 100),
-                99,
-            )
+    if match := re.match(r"^(%[+\-]|[+\-]?)(\d+)$", operators):
+        op = match.group(1)
+        val = int(match.group(2))
+
+        if op == "+":
+            new_value = min(current_value + val, 99)
+        elif op == "-":
+            new_value = max(current_value - val, 0)
+        elif op == "%+":
+            new_value = min(current_value + math.ceil(current_value * val / 100), 99)
+        elif op == "%-":
+            new_value = max(current_value - math.ceil(current_value * val / 100), 0)
         else:
-            new_value = int(match.group(2))
+            new_value = val
         return new_value
 
     # fallback return unchanged value
